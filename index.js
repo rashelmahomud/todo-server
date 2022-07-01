0de4d6fa9570b81ab8873d84e159ad51ffc3457a
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-07-02 00:38:13 +0600

    added setup heroku

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const port = process.env.POST || 5000;
-const app = express()
 
+const port = process.env.PORT || 5000;
+const app = express();
 
-// app.use(cors({
-// origin: "*"
-// }));
+//middleware
 
-
-//modeleware
 app.use(cors());
 app.use(express.json());
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewqse.mongodb.net/?retryWrites=true&w=majority`;
 
